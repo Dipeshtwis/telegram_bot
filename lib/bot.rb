292c0e0ab010448b748fdc3602d6b9a55182a348
Dipeshtwis <deepesh.97kumar@gmail.com>
2020-07-08 16:33:33 +0530

    Add quote file and quote command

--- lib/bot.rb
+++ lib/bot.rb
@@ -1,4 +1,5 @@
 require 'telegram/bot'
+require_relative 'quote.rb'
 class Bot
   def initialize
     token = '1290989050:AAEQZjgmkj64d2voqbECWZJVIeZMP1Mpqnk'
@@ -9,8 +10,12 @@ class Bot
         when '/start'
           bot.api.send_message(chat_id: message.chat.id, text: "Hello, #{message.from.first_name}")
         when '/stop'
-          bot.api.send_message(chat_id: message.chat.id, text: "Bye, #{message.from.first_name}")
+          bot.api.send_message(chat_id: message.chat.id, text: "Sorry to see you go\nBye, #{message.from.first_name}")
+        when '/quote'
+        	quote = Quote.new
+        	bot.api.send_message(chat_id: message.chat.id, text: "Here is a motivational quote for you \n #{quote['text']}", date: message.date)
         end
+
       end
     end
   end
---
+++ lib/quote.rb
@@ -0,0 +1,8 @@
+require 'telegram/bot'
+require 'net/http'
+require 'json'
+require_relative 'bot.rb'
+
+class Quote
+	
+end
